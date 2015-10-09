5e50a95e4b2ddfe64eb3fb2ba69b2f8254273db1
NaisD0128 <nagumo@nais21.co.jp>
2015-10-09 12:44:01 +0900

    旅行ソース

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -3,4 +3,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 
 //kmlì«Ç›çûÇ›
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://naisd0128.github.io/cesium-tokyo/cesium-starter-app-master/Source/test_tokyo.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://naisd0128.github.io/cesium-tokyo/cesium-starter-app-master/Source/travel.kml'));
